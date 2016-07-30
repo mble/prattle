42a55968cc51ae16ec14b35ed6144123e2c16b99
Matt Blewitt <blewitt.matt@gmail.com>
2016-07-30 13:02:24 +0100

    Add setup script, seed data
    * Add setup script with warning around PhantomJS installation
    * Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+20.times do
+  date1 = Time.current
+  date2 = Time.current - 15.months
+  created = Time.zone.at (date2.to_f - date1.to_f) * rand + date1.to_f
+  Post.create content: FFaker::Lorem.sentence, created_at: created
+end
